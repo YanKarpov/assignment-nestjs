5cb0f1ea8c1a7c1c461d4a86de10dea6ca30447a
YanKarpov <KarpovYAA23@spb.ithub.ru>
2025-03-28 10:06:14 +0300

    играюсь с get&set в swagger

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,16 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsArray, IsNotEmpty, IsString, ValidateNested } from "class-validator";
 import { Type } from "class-transformer";
 
 class ArticleDto {
+
+  @ApiProperty()
   @IsNotEmpty()
+  @IsString()
   title: string;
 
+  @ApiProperty()
   @IsNotEmpty()
+  @IsString()
   description: string;
 
+  @ApiProperty()
   @IsNotEmpty()
+  @IsString()
   body: string;
 
+  @ApiProperty()
   @IsArray()
   tagList: string[] = []
 }
